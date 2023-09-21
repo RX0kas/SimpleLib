cdba02bb9470788ef983337c9c3acb7c6a9c2830
ninjagoku4560 <116596186+ninjagoku4560@users.noreply.github.com>
2023-09-21 19:33:12 +0200

    Create Files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SimpleLib',
+    packages=find_packages(include=['SimpleLib']),
+    version='0.1.0',
+    description='A library to facilitate the python development',
+    author='ninjaoku4560',
+    license='MIT',
+)
